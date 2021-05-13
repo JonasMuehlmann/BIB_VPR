14d2f75485c07faca3ffe3ab9c7aeb14098eabca
JayKim94 <jawoon.kim@edu.bib.de>
2021-05-13 21:38:41 +0100

    completed sample CRUD

--- Messenger/Messenger.Core/Services/AzureTestDataService.cs
+++ Messenger/Messenger.Core/Services/AzureTestDataService.cs
@@ -18,28 +18,65 @@ namespace Messenger.Core.Services
 
     public class AzureTestDataService : AzureServiceBase
     {
+        public async Task<int> CreateTeam(string teamName, string teamDescription)
+        {
+            // Create query
+            string query = @"INSERT INTO Teams (TeamName, TeamDescription, CreationDate) "
+                + $"VALUES (@TeamName, @TeamDescription, @CreationDate)";
+
+            try
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    // Open connection
+                    await connection.OpenAsync();
+
+                    // Create Sql-command
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Replace parameters with values
+                    command.Parameters.Add("@TeamName", SqlDbType.NVarChar, 64).Value = teamName;
+                    command.Parameters.Add("@TeamDescription", SqlDbType.NVarChar, 64).Value = teamDescription;
+                    command.Parameters.Add("@CreationDate", SqlDbType.Date).Value = DateTime.Now;
+
+                    // Returns 1 if successful
+                    return command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Database Exception: {e.Message}/{e.InnerException?.Message}");
+                return 0;
+            }
+        }
+
         /// <summary>
         /// Gets list of all teams
         /// </summary>
         /// <returns></returns>
         public async Task<IEnumerable<SampleTeam>> GetAllTeams()
         {
+            // Create query
             const string query = @"SELECT * FROM Teams";
 
             try
             {
                 using (SqlConnection connection = GetConnection())
                 {
+                    // Open connection
                     await connection.OpenAsync();
 
+                    // Create adapter
                     SqlDataAdapter adapter = new SqlDataAdapter(query, connection);
-                    DataSet dataSet = new DataSet();
 
+                    // Fill into dataset
+                    DataSet dataSet = new DataSet();
                     adapter.Fill(dataSet, "Teams");
 
                     List<SampleTeam> teams = new List<SampleTeam>();
                     foreach (DataRow row in dataSet.Tables["Teams"].Rows)
                     {
+                        // Convert to model
                         teams.Add(new SampleTeam()
                         {
                             TeamId = Convert.ToInt32(row["TeamId"]),
@@ -56,6 +93,35 @@ namespace Messenger.Core.Services
             {
                 Debug.WriteLine($"Database Exception: {e.Message}/{e.InnerException?.Message}");
                 return null;
+            }
+        }
+
+        /// <summary>
+        /// Updates team with given id
+        /// </summary>
+        /// <param name="team">team object to update with</param>
+        /// <returns>1(Success)/0(Fail)</returns>
+        public async Task<int> UpdateTeam(SampleTeam team)
+        {
+            string query = $"UPDATE Teams SET TeamName={team.TeamName}, TeamDescription={team.TeamDescription}"
+                + $"WHERE TeamId={team.TeamId}";
+
+            try
+            {
+                using (SqlConnection connection = GetConnection())
+                {
+                    await connection.OpenAsync();
+
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    // Returns 1 if successful
+                    return command.ExecuteNonQuery();
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine($"Database Exception: {e.Message}/{e.InnerException?.Message}");
+                return 0;
             }
         }
 
