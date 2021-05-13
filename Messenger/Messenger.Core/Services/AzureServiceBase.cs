b9ddc99f17236e69f9a2f74b233f625366a958e5
JayKim94 <jawoon.kim@edu.bib.de>
2021-05-13 20:08:54 +0100

    Azure SQL-Transaction Samples

---
+++ Messenger/Messenger.Core/Services/AzureServiceBase.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Messenger.Core.Services
+{
+    public abstract class AzureServiceBase
+    {
+        private string connectionString => ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        protected SqlConnection GetConnection() => new SqlConnection(connectionString);
+    }
+}
--- Messenger/Messenger/ViewModels/MainViewModel.cs
+++ Messenger/Messenger/ViewModels/MainViewModel.cs
@@ -1,13 +1,22 @@
 ﻿using System;
-
+using Messenger.Core.Helpers;
+using Messenger.Core.Services;
 using Messenger.Helpers;
 
 namespace Messenger.ViewModels
 {
     public class MainViewModel : Observable
     {
+        private AzureTestDataService testDataService => Singleton<AzureTestDataService>.Instance;
+
         public MainViewModel()
         {
+            InitalizeData();
+        }
+
+        private async void InitalizeData()
+        {
+            await testDataService.GetAllTeams();
         }
     }
 }
