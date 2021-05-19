6e0c0a4698e71b29989c8365e58257bb9725c556
charlie <charlie.oliver.wiegand@gmail.com>
2021-05-19 11:54:06 +0200

    added comments

--- Messenger/Messenger.Core/Services/PrivateChatService.cs
+++ Messenger/Messenger.Core/Services/PrivateChatService.cs
@@ -14,18 +14,35 @@ namespace Messenger.Core.Services
 {
     public class PrivateChatService : TeamService
     {
+        /// <summary>
+        /// Calls CreatePrivateChat Method an adds the creator and one other user to the chat
+        /// </summary>
+        /// <param name="myUserId">user-Id of the Creator</param>
+        /// <param name="otherUserId">user-Id of the other Person</param>
+        /// <returns>True if no exceptions occured while executing the query, false otherwise</returns>
         public async Task<bool> CreatePrivateChat(string myUserId, string otherUserId)
         {
             int teamID = CreatePrivateChatTeam();
             
            // Add myself and other user as members
-           AddMember(myUserId, teamID);
-           AddMember(otherUserId, teamID);
-           
-           return false;
+           try
+           {
+               AddMember(myUserId, teamID);
+               AddMember(otherUserId, teamID);
+           }catch (Exception e)
+           {
+               Debug.WriteLine($"Database Exception: {e.Message}");
+
+               return false;
+           }
+           return true;
         }
         
 
+        // <summary>
+        /// Creates the PrivateChat
+        /// </summary>
+        /// <returns>The teamId of the created Team</returns>
         public  int CreatePrivateChatTeam()
         {
             int teamID = -1;
@@ -33,7 +50,7 @@ namespace Messenger.Core.Services
                            $"( GETDATE());"
                            + "SELECT CAST(SCOPE_IDENTIDY() AS int)";
             
-            //await SqlHelpers.NonQueryAsync(query, GetConnection());
+            
             SqlCommand command = new SqlCommand(query, GetConnection());
             teamID = (int)command.ExecuteScalar();
             return teamID;
