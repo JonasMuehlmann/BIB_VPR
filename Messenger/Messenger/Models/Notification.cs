6d38ab6e1033866f98d0a29024536d06ffad094f
Robin Brockhaus <robin.brockhaus@zihsler.de>
2021-05-19 15:03:04 +0200

    Linus changes

---
+++ Messenger/Messenger/Models/Notification.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger.Models
+{
+    class Notification
+    {
+        public string User { get; set; }
+        public DateTime whenSent { get; set; }
+        public string kindOfMessage { get; set; }
+        public string whereSent { get; set; }
+
+        public Notification(string user, DateTime whenSent, string kindOfMessage, string whereSent)
+        {
+            User = user;
+            this.whenSent = whenSent;
+            this.kindOfMessage = kindOfMessage;
+            this.whereSent = whereSent;
+        }
+    }
+}
