a13491aef90838cd55424bde435a1b8eb638cdd2
Jay Kim <jawoon.kim@edu.bib.de>
2021-05-23 22:56:51 +0100

    added Membership model

---
+++ Messenger/Messenger.Core/Models/Membership.cs
@@ -0,0 +1,13 @@
+﻿namespace Messenger.Core.Models
+{
+    public class Membership
+    {
+        public int MembershipId { get; set; }
+        
+        public string UserId { get; set; }
+        
+        public int TeamId { get; set; }
+
+        public string UserRole { get; set; }
+    }
+}
