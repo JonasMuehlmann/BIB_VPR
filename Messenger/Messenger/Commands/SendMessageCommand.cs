31b899d29e2c5f4c525651ab791f7068b1152333
Jay Kim <jawoon.kim@edu.bib.de>
2021-05-28 17:06:50 +0100

    SendMessageCommand now uses MessengerService instead of SignalRService

--- Messenger/Messenger/Commands/SendMessageCommand.cs
+++ Messenger/Messenger/Commands/SendMessageCommand.cs
@@ -15,12 +15,12 @@ namespace Messenger.Commands
     public class SendMessageCommand : ICommand
     {
         private readonly ChatHubViewModel _viewModel;
-        private readonly SignalRService _signalRService;
+        private readonly MessengerService _messengerService;
 
-        public SendMessageCommand(ChatHubViewModel viewModel, SignalRService signalRService)
+        public SendMessageCommand(ChatHubViewModel viewModel, MessengerService messengerService)
         {
             _viewModel = viewModel;
-            _signalRService = signalRService;
+            _messengerService = messengerService;
         }
 
         public event EventHandler CanExecuteChanged;
@@ -47,7 +47,7 @@ namespace Messenger.Commands
                     RecipientId = _viewModel.CurrentTeamId
                 };
 
-                await _signalRService.SendMessage(message);
+                await _messengerService.SendMessage(message);
 
                 _viewModel.ErrorMessage = string.Empty;
             }
