95139b9cebc34aa6814a17bc7c9dbc9101fb835e
charlie <charlie.oliver.wiegand@gmail.com>
2021-05-31 12:31:20 +0200

    REFACTOR combining creation and adding members

--- Messenger/Messenger.Core/Services/PrivateChatService.cs
+++ Messenger/Messenger.Core/Services/PrivateChatService.cs
@@ -15,45 +15,34 @@ namespace Messenger.Core.Services
     public class PrivateChatService : TeamService
     {
         /// <summary>
-        /// Calls CreatePrivateChat Method an adds the creator and one other user to the chat
+        /// Create a private chat between the users identified by myUserId and otherUserId.
         /// </summary>
         /// <param name="myUserId">user-Id of the Creator</param>
         /// <param name="otherUserId">user-Id of the other Person</param>
-        /// <returns>True if no exceptions occured while executing the query, false otherwise</returns>
-        public async Task<bool> CreatePrivateChat(string myUserId, string otherUserId)
-        {
-            int teamID = CreatePrivateChatTeam();
-            
-           // Add myself and other user as members
-           try
-           {
-               AddMember(myUserId, teamID);
-               AddMember(otherUserId, teamID);
-           }catch (Exception e)
-           {
-               Debug.WriteLine($"Database Exception: {e.Message}");
-
-               return false;
-           }
-           return true;
-        }
-        
-
-        // <summary>
-        /// Creates the PrivateChat
-        /// </summary>
         /// <returns>The teamId of the created Team</returns>
-        public  int CreatePrivateChatTeam()
+        public async Task<int> CreatePrivateChat(string myUserId, string otherUserId)
         {
             int teamID = -1;
-            string query = $"INSERT INTO Teams (CreationDate) VALUES " +
-                           $"( GETDATE());"
-                           + "SELECT CAST(SCOPE_IDENTIDY() AS int)";
+            // Add myself and other user as members
+           try
+           {
+               string query = $"INSERT INTO Teams (CreationDate) VALUES " +
+                              $"( GETDATE());"
+                              + "SELECT CAST(SCOPE_IDENTITY() AS int)";
             
             
-            SqlCommand command = new SqlCommand(query, GetConnection());
-            teamID = (int)command.ExecuteScalar();
-            return teamID;
+               SqlCommand command = new SqlCommand(query, GetConnection());
+               teamID = (int)command.ExecuteScalar();
+               
+               AddMember(myUserId, teamID);
+               AddMember(otherUserId, teamID);
+           }catch (SqlException e)
+           {
+               Debug.WriteLine($"Database Exception: {e.Message}");
+           }
+           return teamID;
         }
+        
+        
     }
 }
