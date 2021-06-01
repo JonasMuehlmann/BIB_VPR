30d8bc35104c2f52b3122dad25b8026eb3621ce8
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-01 23:46:59 +0100

    ChatViewModel uses ChatHubViewModel

--- Messenger/Messenger/ViewModels/MainPanels/ChatViewModel.cs
+++ Messenger/Messenger/ViewModels/MainPanels/ChatViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-
+using Messenger.Core.Helpers;
 using Messenger.Helpers;
+using Messenger.Services;
 using Windows.UI.Xaml.Navigation;
 
 namespace Messenger.ViewModels
@@ -8,17 +9,40 @@ namespace Messenger.ViewModels
     public class ChatViewModel : Observable
     {
         private ShellViewModel _shellViewModel;
-        public ShellViewModel ShellViewModel { get {
+
+        public ShellViewModel ShellViewModel
+        {
+            get
+            {
                 return _shellViewModel;
             }
-            set {
+            set
+            {
                 _shellViewModel = value;
-                _shellViewModel.ChatName = "Chatname";
-            } }
+                _shellViewModel.ChatName = "ChatName";
+            }
+        }
+
+        public ChatHubViewModel Hub => Singleton<ChatHubViewModel>.Instance;
+
+        private UserDataService UserDataService => Singleton<UserDataService>.Instance;
+
+        private UserViewModel _user;
+
+        public UserViewModel User
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
 
         public ChatViewModel()
         {
+            LoadAsync();
         }
 
+        public async void LoadAsync()
+        {
+            User = await UserDataService.GetUserAsync();
+        }
     }
 }
