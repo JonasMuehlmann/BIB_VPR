76a6b567faa7f0b50ff96e8c6015760a7949e26e
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-01 18:30:07 +0100

    set up dependency injection with Serilog registered

--- Messenger/Messenger.ConsoleMessenger/Program.cs
+++ Messenger/Messenger.ConsoleMessenger/Program.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 using System;
 using System.IO;
@@ -12,15 +14,33 @@ namespace Messenger.ConsoleMessenger
             var builder = new ConfigurationBuilder();
             BuildConfig(builder);
 
+            // Create Serilog with the configuration
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Build())
                 .Enrich.FromLogContext()
                 .WriteTo.Console()
                 .CreateLogger();
 
-            Log.Logger.Information("Application starting");
+            Log.Logger.Information("Setting up the application...");
+
+            // Register services to Host (dependency injection)
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                    services.AddTransient<IMessengerApp, MessengerApp>();
+                })
+                .UseSerilog()
+                .Build();
+
+            // Create the instance of MessengerApp with the dependencies
+            var app = ActivatorUtilities.CreateInstance<MessengerApp>(host.Services);
+            app.Run();
         }
 
+        /// <summary>
+        /// Loads the configuration settings for the application
+        /// </summary>
+        /// <param name="builder">An instance of ConfigurationBuilder</param>
         static void BuildConfig(IConfigurationBuilder builder)
         {
             builder.SetBasePath(Directory.GetCurrentDirectory())
---
+++ Messenger/Messenger.ConsoleMessenger/services/MessengerApp.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Messenger.ConsoleMessenger
+{
+    public class MessengerApp : IMessengerApp
+    {
+        private readonly ILogger<MessengerApp> _log;
+
+        public MessengerApp(ILogger<MessengerApp> log)
+        {
+            _log = log;
+            _config = config;
+        }
+
+        public void Run()
+        {
+            _log.LogInformation("Application starting");
+        }
+    }
+}
