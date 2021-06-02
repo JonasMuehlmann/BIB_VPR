5189ad07a953d1a48b10be6e6290b91d60b07dd9
Linus-Alexander Steinert <linus-alexander.steinert@edu.bib.de>
2021-06-02 15:12:31 +0200

    UI TeamManagment Navigation

--- Messenger/Messenger/Views/MainPanels/TeamManagePage.xaml.cs
+++ Messenger/Messenger/Views/MainPanels/TeamManagePage.xaml.cs
@@ -14,6 +14,9 @@ namespace Messenger.Views
         public TeamManagePage()
         {
             InitializeComponent();
+
+            AddMember.BorderThickness = new Windows.UI.Xaml.Thickness(0,0,0,3);
+            RemoveMember.BorderThickness = new Windows.UI.Xaml.Thickness(0, 0, 0, 0);
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -26,5 +29,16 @@ namespace Messenger.Views
             //Console.WriteLine(e.Parameter as string);
         }
 
+        private void AddMember_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            RemoveMember.BorderThickness = new Windows.UI.Xaml.Thickness(0, 0, 0, 0);
+            AddMember.BorderThickness = new Windows.UI.Xaml.Thickness(0, 0, 0, 3);
+        }
+
+        private void RemoveMember_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            AddMember.BorderThickness = new Windows.UI.Xaml.Thickness(0, 0, 0, 0);
+            RemoveMember.BorderThickness = new Windows.UI.Xaml.Thickness(0, 0, 0, 3);
+        }
     }
 }
