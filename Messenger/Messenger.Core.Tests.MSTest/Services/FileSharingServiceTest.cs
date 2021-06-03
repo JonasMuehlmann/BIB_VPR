6dfd3704804ded9949adb22266ef7fafee58eb82
jonas <jonasmuehlmann@protonmail.com>
2021-06-03 23:33:19 +0200

    test(Core.Services.Tests): Add `FileSharingService` tests

---
+++ Messenger/Messenger.Core.Tests.MSTest/Services/FileSharingServiceTest.cs
@@ -0,0 +1,43 @@
+using System.Threading.Tasks;
+using System.Linq;
+using System;
+using System.Data.SqlClient;
+using Messenger.Core.Models;
+using Messenger.Core.Services;
+using Messenger.Core.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Messenger.Tests.MSTest
+{
+    [TestClass]
+    public class FileSharingServiceTest : SqlServiceTestBase
+    {
+        FileSharingService fileSharingService;
+
+        /// <summary>
+        /// Initialize the service
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            fileSharingService = InitializeTestMode<FileSharingService>();
+        }
+
+        [TestMethod]
+        public void UploadDownload_test()
+        {
+            Task.Run(async () =>
+            {
+                string fileName = await fileSharingService.Upload(@"/home/jonas/test.txt");
+
+                bool success = await fileSharingService.Download(fileName);
+
+                Assert.IsNotNull(fileName);
+                Assert.IsTrue(success);
+
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
