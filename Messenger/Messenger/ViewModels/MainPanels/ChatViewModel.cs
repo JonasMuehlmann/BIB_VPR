8b38c5e97b3420c213cd8977c137f1da0ed3acf9
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-04 10:37:29 +0100

    suppressed xaml build errors

--- Messenger/Messenger/ViewModels/MainPanels/ChatViewModel.cs
+++ Messenger/Messenger/ViewModels/MainPanels/ChatViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
 using Messenger.Core.Helpers;
+using Messenger.Core.Models;
 using Messenger.Helpers;
 using Messenger.Services;
 using Windows.UI.Xaml.Navigation;
@@ -9,6 +13,7 @@ namespace Messenger.ViewModels
     public class ChatViewModel : Observable
     {
         private ShellViewModel _shellViewModel;
+        private ChatHubService Hub => Singleton<ChatHubService>.Instance;
 
         public ShellViewModel ShellViewModel
         {
@@ -23,26 +28,53 @@ namespace Messenger.ViewModels
             }
         }
 
-        public ChatHubViewModel Hub => Singleton<ChatHubViewModel>.Instance;
+        private ObservableCollection<Message> _messages;
 
-        private UserDataService UserDataService => Singleton<UserDataService>.Instance;
-
-        private UserViewModel _user;
-
-        public UserViewModel User
+        public ObservableCollection<Message> Messages
         {
-            get { return _user; }
-            set { _user = value; }
+            get { return _messages; }
+            set
+            {
+                _messages = value;
+                Set(ref _messages, value);
+            }
         }
+
+        public ICommand SendMessageCommand => new RelayCommand<string>(SendMessage);
 
         public ChatViewModel()
         {
+            Messages = new ObservableCollection<Message>();
+            Hub.MessageReceived += OnMessageReceived;
             LoadAsync();
         }
 
-        public async void LoadAsync()
+        private async void LoadAsync()
         {
-            User = await UserDataService.GetUserAsync();
+            if (Hub.CurrentTeamId == null) Hub.CurrentTeamId = 1;
+            UpdateView(await Hub.GetMessages());
+        }
+
+        private void UpdateView(IEnumerable<Message> messages)
+        {
+            Messages.Clear();
+            foreach (var message in messages)
+            {
+                Messages.Add(message);
+            }
+        }
+
+        private async void SendMessage(string content)
+        {
+            await Hub.SendMessage(content);
+        }
+
+        private void OnMessageReceived(object sender, Message message)
+        {
+            if (message.RecipientId == Hub.CurrentTeamId)
+            {
+                Messages.Add(message);
+            }
         }
     }
 }
