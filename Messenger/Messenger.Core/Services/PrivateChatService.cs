1c28ba0ce92ba96befe849d3ccadbfd969d2fdb5
jonas <jonasmuehlmann@protonmail.com>
2021-06-05 18:54:36 +0200

    feat(Core.Services): add logging for `PrivateChatService`

--- Messenger/Messenger.Core/Services/PrivateChatService.cs
+++ Messenger/Messenger.Core/Services/PrivateChatService.cs
@@ -22,27 +22,42 @@ namespace Messenger.Core.Services
         /// <returns>The teamId of the created Team</returns>
         public async Task<uint?> CreatePrivateChat(string myUserId, string otherUserId)
         {
+            Serilog.Context.LogContext.PushProperty("Method","CreatePrivateChat");
+            Serilog.Context.LogContext.PushProperty("SourceContext", this.GetType().Name);
+            logger.Information($"Function called with parameters myUserId={myUserId}, otherUserId={otherUserId}");
+
             uint teamID;
             // Add myself and other user as members
            try
            {
-               string query = $"INSERT INTO Teams (CreationDate) VALUES " +
-                              $"( GETDATE());"
-                              + "SELECT CAST(SCOPE_IDENTITY() AS int)";
-            
-            
-               SqlCommand command = new SqlCommand(query, GetConnection());
-               var team = command.ExecuteScalar();
+                string query = $"INSERT INTO Teams (CreationDate) VALUES "
+                             + $"( GETDATE());"
+                             + "SELECT CAST(SCOPE_IDENTITY() AS int)";
 
-               teamID = SqlHelpers.TryConvertDbValue(team, Convert.ToUInt32);              
-               await AddMember(myUserId, teamID);
-               await AddMember(otherUserId, teamID);
-           }catch (SqlException e)
-           {
-               Debug.WriteLine($"Database Exception: {e.Message}");
-               return null;
+
+                SqlCommand command = new SqlCommand(query, GetConnection());
+
+                logger.Information($"Running the following query: {query}");
+
+                var team = command.ExecuteScalar();
+
+                teamID = SqlHelpers.TryConvertDbValue(team, Convert.ToUInt32);
+
+                logger.Information($"teamID has been determined as {teamID}");
+
+                await AddMember(myUserId, teamID);
+                await AddMember(otherUserId, teamID);
+
+                logger.Information($"Return value: {teamID}");
+
+                return teamID;
            }
-           return teamID;
+           catch (SqlException e)
+           {
+                logger.Information(e, $"Return value: null");
+
+                return null;
+           }
         }
 
 
@@ -52,8 +67,12 @@ namespace Messenger.Core.Services
         /// <returns>An enumerable of Team objects</returns>
         public async Task<IEnumerable<Team>> GetAllPrivateChats()
         {
-            string query = @"SELECT t.TeamId, t.CreationDate 
-                            FROM Teams t,  Messages m 
+            Serilog.Context.LogContext.PushProperty("Method","GetAllPrivateChats");
+            Serilog.Context.LogContext.PushProperty("SourceContext", this.GetType().Name);
+            logger.Information($"Function called");
+
+            string query = @"SELECT t.TeamId, t.CreationDate
+                            FROM Teams t,  Messages m
                             WHERE TeamName IS NULL;
                             AND t.TeamId = m.RecipientId
                             ORDER BY m.CeationTime DESC;";
@@ -64,13 +83,19 @@ namespace Messenger.Core.Services
                 {
                     await connection.OpenAsync();
 
-                    return SqlHelpers
-                        .MapToList(Mapper.TeamFromDataRow, new SqlDataAdapter(query, connection));
+                    logger.Information($"Running the following query: {query}");
+
+                    var result = SqlHelpers.MapToList(Mapper.TeamFromDataRow,
+                                                      new SqlDataAdapter(query, connection));
+                    logger.Information($"Return value: {result}");
+
+                    return result;
                 }
             }
             catch (SqlException e)
             {
-                HandleException(e);
+                logger.Information(e, $"Return value: null");
+
                 return null;
             }
         }
