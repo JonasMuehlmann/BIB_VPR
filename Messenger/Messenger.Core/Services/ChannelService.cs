c32dd8edc5ec42baa0ccb2d5c891d3d3f64f86e4
jonas <jonasmuehlmann@protonmail.com>
2021-06-06 15:56:09 +0200

    feat(Core.Services): add `ChannelService` skeleton

---
+++ Messenger/Messenger.Core/Services/ChannelService.cs
@@ -0,0 +1,30 @@
+using System;
+using Messenger.Core.Helpers;
+using Messenger.Core.Models;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Messenger.Core.Services
+{
+    public class ChannelService: AzureServiceBase
+    {
+        public async Task<uint?> CreateChannel(string channelName, uint teamId)
+        {
+            ;
+        }
+
+        public async Task<bool> RemoveChannel(uint channelId, uint teamId)
+        {
+            ;
+        }
+
+        public async Task<bool> RenameChannel(string channelName, uint channelId)
+        {
+            ;
+        }
+    }
+}
