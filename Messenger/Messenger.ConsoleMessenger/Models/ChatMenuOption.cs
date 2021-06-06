2474fcc82b82e5eedc27dffb376b58886ad7ebe9
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-06 20:53:41 +0100

    added ChatMenuOption

--- Messenger/Messenger.ConsoleMessenger/Models/ChatMenuOption.cs
+++ Messenger/Messenger.ConsoleMessenger/Models/ChatMenuOption.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Messenger.ConsoleMessenger.Models
+﻿namespace Messenger.ConsoleMessenger.Models
 {
     public enum ChatMenuOption
     {
         Send,
+        View,
         Exit
     }
 }
