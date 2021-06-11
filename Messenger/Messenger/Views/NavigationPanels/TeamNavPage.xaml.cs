d617a94fae2a6e3b1efe8750c94b72dbc8734b4c
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-11 18:24:38 +0100

    modified TeamNavPage to call CreateTeamCommand

--- Messenger/Messenger/Views/NavigationPanels/TeamNavPage.xaml.cs
+++ Messenger/Messenger/Views/NavigationPanels/TeamNavPage.xaml.cs
@@ -28,7 +28,6 @@ namespace Messenger.Views
         {
             SymbolIcon newTeamIcon = (sender as Button).Content as SymbolIcon;
 
-
             if (SearchField.Visibility == Windows.UI.Xaml.Visibility.Visible)
             {
                 SearchField.Visibility = Windows.UI.Xaml.Visibility.Collapsed;
@@ -38,16 +37,6 @@ namespace Messenger.Views
             {
                 SearchField.Visibility = Windows.UI.Xaml.Visibility.Visible;
                 newTeamIcon.Symbol = Symbol.Remove;
-            }
-        }
-
-        private void NewTeam_KeyDown(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
-        {
-            if (e.Key == VirtualKey.Enter)
-            {
-                TextBox tb = sender as TextBox;
-
-                ViewModel.NewTeam(tb.Text, "");
             }
         }
     }
