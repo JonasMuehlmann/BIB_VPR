27ce4a99dcaad3ab54d86fa470a5f0d9df7f21ba
jonas <jonasmuehlmann@protonmail.com>
2021-06-15 20:57:51 +0200

    test(`UserService`): fix test for `SearchUser()`

--- Messenger/Messenger.Core.Tests.MSTest/Services/UserServiceTest.cs
+++ Messenger/Messenger.Core.Tests.MSTest/Services/UserServiceTest.cs
@@ -195,31 +195,31 @@ namespace Messenger.Tests.MSTest
             Task.Run(async () =>
             {
                 List<User> users = new List<User>{
-                                       new User(){Id="Id1", NameId=0, DisplayName="UserName1"}
-                                     , new User(){Id="Id2", NameId=1, DisplayName="UserName1"}
-                                     , new User(){Id="Id3", NameId=0, DisplayName="TheUserName2"}
-                                     , new User(){Id="Id4", NameId=0, DisplayName="AnotherUserName"}
-                                     , new User(){Id="Id5", NameId=0, DisplayName="YetAnotherUserName"}
-                                     , new User(){Id="Id6", NameId=0, DisplayName="ThisIsAUserName"}
-                                     , new User(){Id="Id7", NameId=0, DisplayName="AUserNameThisBe"}
+                                       new User(){Id="Id1", NameId=0, DisplayName="DisplayName1"}
+                                     , new User(){Id="Id2", NameId=1, DisplayName="DisplayName1"}
+                                     , new User(){Id="Id3", NameId=0, DisplayName="TheDisplayName2"}
+                                     , new User(){Id="Id4", NameId=0, DisplayName="AnotherDisplayName"}
+                                     , new User(){Id="Id5", NameId=0, DisplayName="YetAnotherDisplayName"}
+                                     , new User(){Id="Id6", NameId=0, DisplayName="ThisIsADisplayName"}
+                                     , new User(){Id="Id7", NameId=0, DisplayName="ADisplayNameThisBe"}
                                      , new User(){Id="Id8", NameId=0, DisplayName="SomeText"}
                                      , new User(){Id="Id9", NameId=0, DisplayName="Yeet"}
                                      , new User(){Id="Id10", NameId=0, DisplayName="Oi mate"}
-                                     , new User(){Id="Id11", NameId=0, DisplayName="Deez Nuts UserName"}
+                                     , new User(){Id="Id11", NameId=0, DisplayName="Deez Nuts DisplayName"}
                                      , new User(){Id="Id12", NameId=0, DisplayName="  "}
-                                     , new User(){Id="Id13", NameId=0, DisplayName="jdhsjdhjdhjuserNamedksdskdjkdjsk"}
+                                     , new User(){Id="Id13", NameId=0, DisplayName="jdhsjdhjdhjdisplayNamedksdskdjkdjsk"}
                                      , new User(){Id="Id14", NameId=0, DisplayName="ksjdksjdahdj"}
-                                     , new User(){Id="Id15", NameId=0, DisplayName="jdhsjdhjdhj uSeRName dksdskdjkdjsk"}
+                                     , new User(){Id="Id15", NameId=0, DisplayName="jdhsjdhjdhj diSpLaYName dksdskdjkdjsk"}
                                  };
 
-                var userMatchString = "UserName1#000000,UserName1#000001,TheUserName2#000000,AnotherUserName#000000,ThisIsAUserName#000000,AUserNameThisBe#000000,YetAnotherUserName#000000,Deez Nuts UserName#000000,jdhsjdhjdhjuserNamedksdskdjkdjsk#000000,jdhsjdhjdhj uSeRName dksdskdjkdjsk#000000";
+                var userMatchString = "DisplayName1#000000,DisplayName1#000001,TheDisplayName2#000000,AnotherDisplayName#000000,ThisIsADisplayName#000000,ADisplayNameThisBe#000000,YetAnotherDisplayName#000000,Deez Nuts DisplayName#000000,jdhsjdhjdhjdisplayNamedksdskdjkdjsk#000000,jdhsjdhjdhj diSpLaYName dksdskdjkdjsk#000000";
 
                 foreach (var user in users)
                 {
                     Assert.IsNotNull(await userService.GetOrCreateApplicationUser(user));
                 }
 
-                var userMatches = await userService.SearchUser("UserName");
+                var userMatches = await userService.SearchUser("DisplayName");
                 Assert.IsNotNull(userMatches);
 
                 Assert.AreEqual(userMatchString, string.Join(",", userMatches));
