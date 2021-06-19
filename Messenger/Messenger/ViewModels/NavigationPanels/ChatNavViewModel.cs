5edbd156c1f50d621e2419e316684db901e9118e
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-19 22:04:24 +0100

    implemented ChatNavViewModel

--- Messenger/Messenger/ViewModels/NavigationPanels/ChatNavViewModel.cs
+++ Messenger/Messenger/ViewModels/NavigationPanels/ChatNavViewModel.cs
@@ -1,38 +1,136 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Messenger.Core.Helpers;
 using Messenger.Core.Models;
 using Messenger.Helpers;
-using Windows.UI.Xaml.Navigation;
+using Messenger.Services;
+using Messenger.Views.DialogBoxes;
+using Windows.UI.Xaml.Controls;
+using WinUI = Microsoft.UI.Xaml.Controls;
 
 namespace Messenger.ViewModels
 {
     public class ChatNavViewModel : Observable
     {
-        private ShellViewModel _shellViewModel;
-        public ShellViewModel ShellViewModel { get {
-                return _shellViewModel;
-            }
-            set {
-                _shellViewModel = value;
-            } }
+        private ObservableCollection<Team> _chats;
+        private bool _isBusy;
 
-        private User _user;
-        public User User
+        private ChatHubService ChatHubService => Singleton<ChatHubService>.Instance;
+        private UserDataService UserDataService => Singleton<UserDataService>.Instance;
+
+        public ObservableCollection<Team> Chats
         {
             get
             {
-                return _user;
+                return _chats;
             }
-            set { Set(ref _user, value); }
+            set
+            {
+                Set(ref _chats, value);
+            }
         }
+
+        public bool IsBusy
+        {
+            get
+            {
+                return _isBusy;
+            }
+            set
+            {
+                Set(ref _isBusy, value);
+            }
+        }
+
+        public ICommand SwitchChatCommand => new RelayCommand<WinUI.TreeViewItemInvokedEventArgs>(SwitchChat);
+
+        public ICommand StartChatCommand => new RelayCommand(StartChatAsync);
 
         public ChatNavViewModel()
         {
-            User = new User();
-            User.Userlist = new List<string>();
-            User.Userlist.Add("Hello");
-            User.Userlist.Add("HI");
+            IsBusy = true;
+
+            Chats = new ObservableCollection<Team>();
+            ChatHubService.TeamsUpdated += OnTeamsUpdated;
+            LoadAsync();
         }
 
+        private async void LoadAsync()
+        {
+            var user = await UserDataService.GetUserAsync();
+
+            if (user.Teams != null && user.Teams.Count > 0)
+            {
+                FilterAndUpdateChats(user.Teams);
+            }
+
+            IsBusy = false;
+        }
+
+        public async void StartChatAsync()
+        {
+            var dialog = new CreateChatDialog();
+            dialog.OnSearch = SearchUsers;
+            ContentDialogResult result = await dialog.ShowAsync();
+
+            if (result == ContentDialogResult.Primary)
+            {
+                var selectedUser = dialog.SelectedUser;
+
+                await ChatHubService.StartChat(selectedUser.DisplayName, selectedUser.NameId);
+            }
+        }
+
+        private async Task<IList<string>> SearchUsers(string username)
+        {
+            var userStrings = await ChatHubService.SearchUser(username);
+
+            return userStrings;
+        }
+
+        /// <summary>
+        /// Fires on TeamsUpdated in ChatHubService and refreshes the view
+        /// </summary>
+        /// <param name="sender">Service that invoked the event</param>
+        /// <param name="teams">Enumerable of teams</param>
+        private void OnTeamsUpdated(object sender, IEnumerable<Team> teams)
+        {
+            if (teams != null)
+            {
+                FilterAndUpdateChats(teams);
+            }
+
+            IsBusy = false;
+        }
+
+        /// <summary>
+        /// Fires on click and invokes ChatHubService to load messages of the selected team
+        /// </summary>
+        /// <param name="args">Event argument from the event, contains the data of the invoked item</param>
+        private async void SwitchChat(WinUI.TreeViewItemInvokedEventArgs args)
+        {
+            uint teamId = (args.InvokedItem as Team).Id;
+
+            // Invokes TeamSwitched event
+            await ChatHubService.SwitchTeam(teamId);
+        }
+
+        private void FilterAndUpdateChats(IEnumerable<Team> teams)
+        {
+            if (teams != null)
+            {
+                IEnumerable<Team> chatsList = teams.Where(team => string.IsNullOrEmpty(team.Name));
+
+                Chats.Clear();
+                foreach (var chat in chatsList)
+                {
+                    Chats.Add(chat);
+                }
+            }
+        }
     }
 }
