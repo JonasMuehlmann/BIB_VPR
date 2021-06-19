151adae292961c8ed311998f2f544d2c0d0cc8df
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-19 14:03:29 +0100

    added ReplyToCommand

---
+++ Messenger/Messenger/Commands/Messenger/ReplyToCommand.cs
@@ -0,0 +1,35 @@
+﻿using Messenger.Core.Models;
+using Messenger.ViewModels;
+using System;
+using System.Windows.Input;
+
+namespace Messenger.Commands.Messenger
+{
+    public class ReplyToCommand : ICommand
+    {
+        private readonly ChatViewModel _viewModel;
+
+        public event EventHandler CanExecuteChanged;
+
+        public ReplyToCommand(ChatViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            Message message = parameter as Message;
+
+            if (message != null)
+            {
+                _viewModel.ReplyMessage = message;
+                _viewModel.MessageToSend.ParentMessageId = message.Id;
+            }
+        }
+    }
+}
