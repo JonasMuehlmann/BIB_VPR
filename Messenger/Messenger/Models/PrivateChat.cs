aeaa967f38fbafbf8ecf8720ba8ae46b0a971662
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-20 09:43:37 +0100

    added comments in PrivateChat

--- Messenger/Messenger/Models/PrivateChat.cs
+++ Messenger/Messenger/Models/PrivateChat.cs
@@ -3,10 +3,18 @@ using System.Linq;
 
 namespace Messenger.Models
 {
+    /// <summary>
+    /// This extended model of a Team is mainly used to ease bindings in XAML
+    /// </summary>
     public class PrivateChat : Team
     {
         public User Partner { get; set; }
 
+        /// <summary>
+        /// Creates a PrivateChat object from the given team data
+        /// </summary>
+        /// <param name="teamData">Team data from the service</param>
+        /// <returns>A complete PrivateChat object</returns>
         public static PrivateChat CreatePrivateChatFromTeamData(Team teamData)
         {
             bool isMemberDataValid = teamData.Members != null
