6c606006bca362cc8b8e2f048a1479636df3ba8e
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-20 09:30:29 +0100

    added PrivateChat model for front end logics

---
+++ Messenger/Messenger/Models/PrivateChat.cs
@@ -0,0 +1,27 @@
+﻿using Messenger.Core.Models;
+using System.Linq;
+
+namespace Messenger.Models
+{
+    public class PrivateChat : Team
+    {
+        public User Partner { get; set; }
+
+        public static PrivateChat CreatePrivateChatFromTeamData(Team teamData)
+        {
+            bool isMemberDataValid = teamData.Members != null
+                && teamData.Members.Count > 0;
+
+            User partner = isMemberDataValid ? teamData.Members.FirstOrDefault() : null;
+
+            return new PrivateChat()
+            {
+                Id = teamData.Id,
+                Name = string.Empty,
+                Description = teamData.Description,
+                CreationDate = teamData.CreationDate,
+                Partner = partner
+            };
+        }
+    }
+}
