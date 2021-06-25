b701532b28ae7c488551ea2c9c854bb4f1c55656
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-25 10:16:24 +0100

    added MessageControl with placeholder

---
+++ Messenger/Messenger/Controls/ChatControls/MessageControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Messenger.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Die Elementvorlage "Benutzersteuerelement" wird unter https://go.microsoft.com/fwlink/?LinkId=234236 dokumentiert.
+
+namespace Messenger.Controls.ChatControls
+{
+    public sealed partial class MessageControl : UserControl
+    {
+        public Message Message
+        {
+            get { return (Message)GetValue(MessageProperty); }
+            set { SetValue(MessageProperty, value); }
+        }
+
+        public static readonly DependencyProperty MessageProperty =
+            DependencyProperty.Register("Message", typeof(Message), typeof(MessageControl), new PropertyMetadata(null));
+
+        public MessageControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
