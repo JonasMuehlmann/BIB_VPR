03f10519ab77a8593dc198f91eb06f5de6fdc464
jonas <jonasmuehlmann@protonmail.com>
2021-06-26 15:01:27 +0200

    fix: correct order of connection strings

--- Messenger/Messenger.Core/Services/AzureServiceBase.cs
+++ Messenger/Messenger.Core/Services/AzureServiceBase.cs
@@ -10,10 +10,10 @@ namespace Messenger.Core.Services
         #region Private
 
         #if BIB_VPR_DEBUG
-        private static string connectionString = "Server=tcp:bib-vpr.database.windows.net,1433;Initial Catalog=vpr_messenger_database;Persist Security Info=False;User ID=pbt3h19a;Password=uMb7ZXAA5TjajDw;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
-        #else
-        // Testing connection string
+        // Testing cnection string
         private static string connectionString = @"Server=tcp:bib-vpr.database.windows.net,1433;Initial Catalog=vpr_messenger_database_test;Persist Security Info=False;User ID=pbt3h19a;Password=uMb7ZXAA5TjajDw;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        #else
+        private static string connectionString = "Server=tcp:bib-vpr.database.windows.net,1433;Initial Catalog=vpr_messenger_database;Persist Security Info=False;User ID=pbt3h19a;Password=uMb7ZXAA5TjajDw;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
         #endif
 
         public static ILogger logger => GlobalLogger.Instance;
