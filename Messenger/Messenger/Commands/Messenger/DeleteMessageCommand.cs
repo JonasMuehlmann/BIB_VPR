394fff11952c72cf298d5b854d46be5161963c9f
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-27 12:51:15 +0100

    added DeleteMessageCommand

---
+++ Messenger/Messenger/Commands/Messenger/DeleteMessageCommand.cs
@@ -0,0 +1,43 @@
+﻿using Messenger.Core.Helpers;
+using Messenger.Services;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Messenger.Commands.Messenger
+{
+    public class DeleteMessageCommand : ICommand
+    {
+        private readonly ChatHubService _hub;
+        private ILogger _logger => GlobalLogger.Instance;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            bool executable = parameter is uint;
+
+            if (!executable)
+            {
+                return;
+            }
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                _logger.Information($"Error while deleting the message: {e.Message}");
+            }
+        }
+    }
+}
