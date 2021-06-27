92985487f7b7561a4af9fb107141be24d73e19be
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-27 08:06:41 +0100

    added EditMessageCommand file

---
+++ Messenger/Messenger/Commands/Messenger/EditMessageCommand.cs
@@ -0,0 +1,35 @@
+﻿using Messenger.Core.Helpers;
+using Messenger.Services;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Messenger.Commands.Messenger
+{
+    public class EditMessageCommand : ICommand
+    {
+        private readonly ChatHubService _hub;
+        private ILogger _logger => GlobalLogger.Instance;
+
+        public EditMessageCommand(ChatHubService hub)
+        {
+            _hub = hub;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
