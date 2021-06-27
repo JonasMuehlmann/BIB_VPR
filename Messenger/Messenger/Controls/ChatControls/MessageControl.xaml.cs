f112dfb563e7c4b53401457895cfa36f4d3b6ad3
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-27 12:47:52 +0100

    added editable content Textbox in MessageControl

--- Messenger/Messenger/Controls/ChatControls/MessageControl.xaml.cs
+++ Messenger/Messenger/Controls/ChatControls/MessageControl.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Messenger.Core.Models;
+using Messenger.Helpers;
 using Messenger.ViewModels.DataViewModels;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,15 @@ namespace Messenger.Controls.ChatControls
         public static readonly DependencyProperty MessageProperty =
             DependencyProperty.Register("Message", typeof(MessageViewModel), typeof(MessageControl), new PropertyMetadata(null));
 
+        public string EditedContent
+        {
+            get { return (string)GetValue(EditedContentProperty); }
+            set { SetValue(EditedContentProperty, value); }
+        }
+
+        public static readonly DependencyProperty EditedContentProperty =
+            DependencyProperty.Register("EditedContent", typeof(string), typeof(MessageControl), new PropertyMetadata(string.Empty));
+
         public ICommand ReplyToCommand
         {
             get { return (ICommand)GetValue(ReplyToCommandProperty); }
@@ -40,14 +50,76 @@ namespace Messenger.Controls.ChatControls
         public static readonly DependencyProperty ReplyToCommandProperty =
             DependencyProperty.Register("ReplyToCommand", typeof(ICommand), typeof(MessageControl), new PropertyMetadata(null));
 
+        public ICommand EnterEditModeCommand
+        {
+            get { return (ICommand)GetValue(EnterEditModeCommandProperty); }
+            set { SetValue(EnterEditModeCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty EnterEditModeCommandProperty =
+            DependencyProperty.Register("EnterEditModeCommand", typeof(ICommand), typeof(MessageControl), new PropertyMetadata(null));
+
+        public ICommand ExitEditModeCommand
+        {
+            get { return (ICommand)GetValue(ExitEditModeCommandProperty); }
+            set { SetValue(ExitEditModeCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty ExitEditModeCommandProperty =
+            DependencyProperty.Register("ExitEditModeCommand", typeof(ICommand), typeof(MessageControl), new PropertyMetadata(null));
+
+        public ICommand EditMessageCommand
+        {
+            get { return (ICommand)GetValue(EditMessageCommandProperty); }
+            set { SetValue(EditMessageCommandProperty, value); }
+        }
+
+        public static readonly DependencyProperty EditMessageCommandProperty =
+            DependencyProperty.Register("EditMessageCommand", typeof(ICommand), typeof(MessageControl), new PropertyMetadata(null));
+
         public MessageControl()
         {
+            EnterEditModeCommand = new RelayCommand(EnterEditMode);
+            ExitEditModeCommand = new RelayCommand(ExitEditMode);
+
             InitializeComponent();
         }
 
         private void AppBarButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
             FlyoutBase.ShowAttachedFlyout((FrameworkElement)sender);
+        }
+
+        private void EnterEditMode()
+        {
+            EditContent.Visibility = Visibility.Visible;
+            ShowContent.Visibility = Visibility.Collapsed;
+
+            EditedContent = ShowContent.Text;
+        }
+
+        private void ExitEditMode()
+        {
+            EditContent.Visibility = Visibility.Collapsed;
+            ShowContent.Visibility = Visibility.Visible;
+        }
+
+        private void EditAcceptButton_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            if (Message == null)
+            {
+                return;
+            }
+
+            var vm = new MessageViewModel()
+            {
+                Id = Message.Id,
+                Content = NewContentTextBox.Text
+            };
+
+            EditMessageCommand?.Execute(vm);
+
+            ExitEditMode();
         }
     }
 }
