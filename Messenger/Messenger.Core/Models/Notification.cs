860bac84f8ea2a1a0c6422328055fa9772d5f799
jonas <jonasmuehlmann@protonmail.com>
2021-06-28 11:10:14 +0200

    feat(`Models.Notification`): add Id to model

--- Messenger/Messenger.Core/Models/Notification.cs
+++ Messenger/Messenger.Core/Models/Notification.cs
@@ -5,6 +5,7 @@ namespace Messenger.Core.Models
 {
     public class Notification
     {
+        public int Id { get; set; }
         public string RecipientId { get; set; }
         public DateTime CreationTime { get; set; }
         public string Message { get; set; }
