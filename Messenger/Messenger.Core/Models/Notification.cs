09f7da3112a606840eebb42d90c301096450f928
jonas <jonasmuehlmann@protonmail.com>
2021-06-28 10:22:19 +0200

    feat(`Models`): add `Notification` class

---
+++ Messenger/Messenger.Core/Models/Notification.cs
@@ -0,0 +1,23 @@
+using System;
+
+
+namespace Messenger.Core.Models
+{
+    public class Notification
+    {
+        public string RecipientId { get; set; }
+        public DateTime CreationTime { get; set; }
+        public string Message { get; set; }
+
+        public Notification()
+        {
+            RecipientId = "";
+            Message = "";
+        }
+
+        public override string ToString()
+        {
+            return $"Notification: RecipientId={RecipientId}, CreationTime={CreationTime}, Message={Message}";
+        }
+    }
+}
