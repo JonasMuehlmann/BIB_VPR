82c22dd7cf4ee76889c6a4dc829e86a0192f769a
Jay Kim <jawoon.kim@edu.bib.de>
2021-06-29 22:44:48 +0100

    added ReactionTypeToBooleanConverter

---
+++ Messenger/Messenger/Helpers/ReactionTypeToBooleanConverter.cs
@@ -0,0 +1,28 @@
+﻿using Messenger.Models;
+using System;
+using Windows.UI.Xaml.Data;
+
+namespace Messenger.Helpers
+{
+    public class ReactionTypeToBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null
+                || !(value is string))
+            {
+                return false;
+            }
+
+            ReactionType type = (ReactionType)value;
+            ReactionType param = (ReactionType)Enum.Parse(typeof(ReactionType), parameter.ToString());
+
+            return type == param ? true : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
