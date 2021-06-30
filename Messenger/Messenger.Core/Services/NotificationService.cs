fcf18be4fc8b9a7c649d043ccf23b344f2ad961b
jonas <jonasmuehlmann@protonmail.com>
2021-06-30 11:29:03 +0200

    fix(`NotificationSeriver`): adjust parameter of `SendNotification` to new message type

--- Messenger/Messenger.Core/Services/NotificationService.cs
+++ Messenger/Messenger.Core/Services/NotificationService.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Serilog.Context;
 using Messenger.Core.Models;
 using Messenger.Core.Helpers;
+using System.Text.Json;
 
 namespace Messenger.Core.Services
 {
@@ -17,20 +18,19 @@ namespace Messenger.Core.Services
         /// </param>
         /// <param name="message">The message the notification holds</param>
         /// <returns>The id of the notification on success, null otherwise</returns>
-        public async Task<uint?> SendNotification(string recipientId, string message)
+        public async Task<uint?> SendNotification(string recipientId, NotificationMessageBase message)
         {
             LogContext.PushProperty("Method","SendNotification");
             LogContext.PushProperty("SourceContext", this.GetType().Name);
 
             logger.Information($"Function called with parameters recipientId={recipientId}, message={message}");
 
-            // TODO: Create json encoded message from method parameters
             string query = $@"
                                 INSERT INTO
                                     Notifications
                                 VALUES(
                                     '{recipientId}',
-                                    '{message}',
+                                    '{JsonSerializer.Serialize(message)}',
                                     GETDATE()
                                 );
 
