a7c844c46f2ba99f27c9b26547c82bbd935daa81
jonas <jonasmuehlmann@protonmail.com>
2021-07-02 08:29:34 +0200

    test(`NotificationService`): add test

---
+++ Messenger/Messenger.Core.Tests.MSTest/Services/NotificationServiceTest.cs
@@ -0,0 +1,60 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Messenger.Core.Models;
+using Messenger.Core.Services;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Messenger.Tests.MSTest
+{
+    [TestClass]
+    public class NotificationServiceTest
+    {
+        private NotificationService notificationService = new NotificationService();
+        private UserService userService = new UserService();
+        private TeamService teamService = new TeamService();
+        private ChannelService channelService = new ChannelService();
+
+
+        [TestMethod]
+        public void SendNotification_Test()
+        {
+
+            var testName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            Task.Run(async () =>
+            {
+                string receiverId = (await userService.GetOrCreateApplicationUser(new User(){Id = testName + "UserReceiver"})).Id;
+                Assert.IsNotNull(receiverId);
+                Assert.AreNotEqual("",receiverId);
+
+                string senderId = (await userService.GetOrCreateApplicationUser(new User(){Id = testName + "UserSender"})).Id;
+                Assert.IsNotNull(senderId);
+                Assert.AreNotEqual("",senderId);
+
+                uint? teamId = await teamService.CreateTeam(testName + "Team");
+                Assert.IsNotNull(teamId);
+
+                uint? channelId = await channelService.CreateChannel(testName + "Chanel", teamId.Value);
+                Assert.IsNotNull(channelId);
+
+                var notificationMessage = new MessageInSubscribedChannelNotificationMessage()
+                {
+                    NotificationType   = NotificationType.MessageInSubscribedChannel,
+                    NotificationSource = NotificationSource.Channel,
+                    SenderId           = senderId,
+                    TeamId             = teamId.Value,
+                    ChannelId          = channelId.Value
+                };
+                uint? notificationId = await notificationService.SendNotification(receiverId, notificationMessage);
+                Assert.IsNotNull(notificationId);
+
+                var notifications = await notificationService.RetrieveNotifications<NotificationMessageBase>(receiverId);
+                Assert.AreEqual(1, Enumerable.Count(notifications));
+
+            }).GetAwaiter().GetResult();
+        }
+    }
+}
