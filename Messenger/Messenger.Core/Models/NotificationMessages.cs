6a82a187853982717353e90e70a773cd92a0c873
jonas <jonasmuehlmann@protonmail.com>
2021-07-02 11:41:34 +0200

    fix(`NotificationMessages`): add attributes to convert enums to string in json

--- Messenger/Messenger.Core/Models/NotificationMessages.cs
+++ Messenger/Messenger.Core/Models/NotificationMessages.cs
@@ -1,11 +1,14 @@
 using System;
-using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace Messenger.Core.Models
 {
-    public class NotificationMessageBase : IConvertible
+    public class NotificationMessageBase
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public NotificationSource NotificationSource { get; set; }
+        [JsonConverter(typeof(StringEnumConverter))]
         public NotificationType NotificationType { get; set; }
 
         public NotificationMessageBase(){}
@@ -17,57 +20,11 @@ namespace Messenger.Core.Models
             NotificationType = notificationType;
         }
 
-        #region IConvertible Implementation
-
-        static T ThrowNotSupported<T>()
-        {
-            var ex = ThrowNotSupported(typeof(T));
-            return (T)ex;
-        }
-
-        static object ThrowNotSupported(Type type)
-        {
-            throw new InvalidCastException($"Converting type \"{typeof(NotificationMessageBase)}\" to type \"{type}\" is not supported.");
-        }
-
-        TypeCode IConvertible.GetTypeCode()
-        {
-            return TypeCode.Object;
-        }
-
-        bool     IConvertible.ToBoolean(IFormatProvider provider) => ThrowNotSupported<bool>();
-        char     IConvertible.ToChar(IFormatProvider provider) => ThrowNotSupported<char>();
-        sbyte    IConvertible.ToSByte(IFormatProvider provider) => ThrowNotSupported<sbyte>();
-        byte     IConvertible.ToByte(IFormatProvider provider) => ThrowNotSupported<byte>();
-        short    IConvertible.ToInt16(IFormatProvider provider) => ThrowNotSupported<short>();
-        ushort   IConvertible.ToUInt16(IFormatProvider provider) => ThrowNotSupported<ushort>();
-        int      IConvertible.ToInt32(IFormatProvider provider) => ThrowNotSupported<int>();
-        uint     IConvertible.ToUInt32(IFormatProvider provider) => ThrowNotSupported<uint>();
-        long     IConvertible.ToInt64(IFormatProvider provider) => ThrowNotSupported<long>();
-        ulong    IConvertible.ToUInt64(IFormatProvider provider) => ThrowNotSupported<ulong>();
-        float    IConvertible.ToSingle(IFormatProvider provider) => ThrowNotSupported<float>();
-        double   IConvertible.ToDouble(IFormatProvider provider) => ThrowNotSupported<double>();
-        decimal  IConvertible.ToDecimal(IFormatProvider provider) => ThrowNotSupported<decimal>();
-        DateTime IConvertible.ToDateTime(IFormatProvider provider) => ThrowNotSupported<DateTime>();
-        string   IConvertible.ToString(IFormatProvider provider) => ThrowNotSupported<string>();
-
-        object IConvertible.ToType(Type conversionType, IFormatProvider provider)
-        {
-            if (conversionType == typeof(NotificationMessageBase))
-            {
-                return this;
             }
-
-            // Other implementations here
-
-            return ThrowNotSupported(conversionType);
-        }
-
-        #endregion
-    }
 
     public class UserMentionedNotificationMessage : NotificationMessageBase
     {
+        [JsonConverter(typeof(StringEnumConverter))]
         public MentionTarget MentionTarget { get; set; }
         public string MentionTargetName { get; set; }
 
