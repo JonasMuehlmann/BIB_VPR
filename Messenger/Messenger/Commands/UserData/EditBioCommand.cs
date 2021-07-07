a3c1e9273ddaf17710f43006cae6576157c9452c
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-07 18:00:06 +0100

    added EditBioCommand

---
+++ Messenger/Messenger/Commands/UserData/EditBioCommand.cs
@@ -0,0 +1,66 @@
+﻿using Messenger.Core.Helpers;
+using Messenger.Services;
+using Messenger.Views.DialogBoxes;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Messenger.Commands.UserData
+{
+    public class EditBioCommand : ICommand
+    {
+        private readonly ChatHubService _hub;
+
+        private ILogger _logger => GlobalLogger.Instance;
+
+        public EditBioCommand(ChatHubService hub)
+        {
+            _hub = hub;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            bool executable = parameter != null
+                && parameter is string
+                && !string.IsNullOrEmpty(parameter.ToString());
+
+            if (!executable)
+            {
+                return;
+            }
+
+            try
+            {
+                bool isSuccess = await _hub.UpdateUserBio(parameter.ToString());
+
+                if (isSuccess)
+                {
+                    await ResultConfirmationDialog
+                        .Set(true, "Successfully updated the bio")
+                        .ShowAsync();
+                }
+                else
+                {
+                    await ResultConfirmationDialog
+                        .Set(false, "We could not updated the bio")
+                        .ShowAsync();
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.Information($"Error while editing user bio: {e.Message}");
+            }
+        }
+    }
+}
