4a6571bd7260730f48e9641d59b7ad4cd90f209e
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-12 09:44:52 +0100

    implemented TeamBuilder

--- Messenger/Messenger/Helpers/TeamBuilder.cs
+++ Messenger/Messenger/Helpers/TeamBuilder.cs
@@ -4,6 +4,7 @@ using Messenger.ViewModels;
 using Messenger.ViewModels.DataViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +13,58 @@ namespace Messenger.Helpers
 {
     public class TeamBuilder
     {
-        public async Task<TeamViewModel> Build(Team team, UserViewModel currentUser)
+        private readonly UserViewModel _user;
+
+        public TeamBuilder(UserViewModel user)
         {
-            throw new NotImplementedException();
+            _user = user;
+        }
 
-            //var teams = await MessengerService.LoadTeams(currentUser.Id);
+        public async Task<TeamViewModel> Build(Team team)
+        {
+            TeamViewModel viewModel = Map(team);
 
-            //await SetMembers(teams, currentUser.Id);
-            //teams = await GetChannelsForAllTeams(teams);
+            var withMembers = await LoadMembers(viewModel, _user.Id);
+
+            var withChannels = await LoadChannels(withMembers);
+
+            return withChannels;
+        }
+
+        public async Task<List<TeamViewModel>> Build(IEnumerable<Team> teams)
+        {
+            List<TeamViewModel> result = new List<TeamViewModel>();
+
+            foreach (Team team in teams)
+            {
+                var viewModel = await Build(team);
+                result.Add(viewModel);
+            }
+
+            return result;
+        }
+
+        private TeamViewModel Map(Team team)
+        {
+            return new TeamViewModel()
+            {
+                Id = team.Id,
+                TeamName = team.Name,
+                Description = team.Description,
+                CreationDate = team.CreationDate,
+                Members = new ObservableCollection<User>(),
+                Channels = new ObservableCollection<ChannelViewModel>()
+            };
+        }
+
+        private ChannelViewModel Map(Channel channel)
+        {
+            return new ChannelViewModel()
+            {
+                ChannelId = channel.ChannelId,
+                TeamId = channel.TeamId,
+                ChannelName = channel.ChannelName
+            };
         }
 
         /// <summary>
@@ -27,25 +72,34 @@ namespace Messenger.Helpers
         /// </summary>
         /// <param name="team">Team object to reference to</param>
         /// <param name="members">List of members to set</param>
-        private async Task SetMembers(IEnumerable<Team> teams, string currentUserId)
+        private async Task<TeamViewModel> LoadMembers(TeamViewModel viewModel, string currentUserId)
         {
-            throw new NotImplementedException();
+            var members = await MessengerService.LoadTeamMembers((uint)viewModel.Id);
 
-            //foreach (Team team in teams)
-            //{
-            //    var members = await MessengerService.LoadTeamMembers(team.Id);
-            //    // If it is a private chat, exclude current user id
-            //    if (string.IsNullOrEmpty(team.Name))
-            //    {
-            //        team.Members = members
-            //            .Where(m => m.Id != currentUserId)
-            //            .ToList();
-            //    }
-            //    else
-            //    {
-            //        team.Members = members.ToList();
-            //    }
-            //}
+            if (string.IsNullOrEmpty(viewModel.TeamName))
+            {
+                viewModel.Members = new ObservableCollection<User>(members.Where(m => m.Id != currentUserId));
+            }
+
+            return viewModel;
+        }
+
+        private async Task<TeamViewModel> LoadChannels(TeamViewModel viewModel)
+        {
+            var channels = await MessengerService.GetChannelsForTeam((uint)viewModel.Id);
+
+            if (channels.Count() > 0)
+            {
+                var channelViewModels = channels.Select(Map);
+
+                viewModel.Channels.Clear();
+                foreach (ChannelViewModel channelViewModel in channelViewModels)
+                {
+                    viewModel.Channels.Add(channelViewModel);
+                }
+            }
+
+            return viewModel;
         }
     }
 }
