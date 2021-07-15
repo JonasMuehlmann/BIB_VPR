36eb14a84a8ed36c05202907d53dcafe40ab1617
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-15 19:14:00 +0100

    added MemberRole model

---
+++ Messenger/Messenger/Models/MemberRole.cs
@@ -0,0 +1,38 @@
+﻿using Messenger.Core.Models;
+using Messenger.Helpers;
+using System.Collections.Generic;
+
+namespace Messenger.Models
+{
+    public class MemberRole : Observable
+    {
+        private string _title;
+
+        private IList<Permissions> _permissions;
+
+        private uint _teamId;
+
+        public string Title
+        {
+            get { return _title; }
+            set { Set(ref _title, value); }
+        }
+
+        public IList<Permissions> Permissions
+        {
+            get { return _permissions; }
+            set { Set(ref _permissions, value); }
+        }
+
+        public uint TeamId
+        {
+            get { return _teamId; }
+            set { Set(ref _teamId, value); }
+        }
+
+        public MemberRole()
+        {
+            Permissions = new List<Permissions>();
+        }
+    }
+}
