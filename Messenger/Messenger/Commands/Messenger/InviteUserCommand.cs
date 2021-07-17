0c3444c6bc90b9d1768c4e7b1d06d5cf0815055b
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-17 20:35:50 +0100

    modified InviteUserCommand to directly reflect the changes to UI

--- Messenger/Messenger/Commands/Messenger/InviteUserCommand.cs
+++ Messenger/Messenger/Commands/Messenger/InviteUserCommand.cs
@@ -3,6 +3,7 @@ using Messenger.Core.Models;
 using Messenger.Models;
 using Messenger.Services;
 using Messenger.ViewModels;
+using Messenger.Views.DialogBoxes;
 using Serilog;
 using System;
 using System.Windows.Input;
@@ -12,10 +13,12 @@ namespace Messenger.Commands.Messenger
     public class InviteUserCommand : ICommand
     {
         private readonly ChatHubService _hub;
+        private readonly TeamManageViewModel _viewModel;
         private ILogger _logger = GlobalLogger.Instance;
 
-        public InviteUserCommand(ChatHubService hub)
+        public InviteUserCommand(TeamManageViewModel viewModel, ChatHubService hub)
         {
+            _viewModel = viewModel;
             _hub = hub;
         }
 
@@ -45,12 +48,27 @@ namespace Messenger.Commands.Messenger
                         UserId = (parameter as TeamManageViewModel).SelectedSearchedUser.Id
                     };
 
-                    bool isSuccess = await _hub.InviteUser(invitation);
+                    Member member = await _hub.InviteUser(invitation);
+
+                    if (member != null)
+                    {
+                        _viewModel.Members.Add(member);
+                    }
+                    else
+                    {
+                        await ResultConfirmationDialog
+                            .Set(false, "We could not invite the user, try again.")
+                            .ShowAsync();
+                    }
                 }
             }
             catch (Exception e)
             {
                 _logger.Information($"Error while inviting a user: {e.Message}");
+
+                await ResultConfirmationDialog
+                            .Set(false, "We could not invite the user, try again.")
+                            .ShowAsync();
             }
         }
     }
