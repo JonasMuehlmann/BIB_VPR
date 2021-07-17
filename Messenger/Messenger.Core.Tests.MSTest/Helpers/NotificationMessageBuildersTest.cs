9e2e2ae27a5823a39efe77359ca50db812513afa
jonas <jonasmuehlmann@protonmail.com>
2021-07-17 17:11:13 +0200

    test(`NotificationMessageBuilder`): add test for `MakeMessageInSubscribedTeamNotificationMessage()`

--- Messenger/Messenger.Core.Tests.MSTest/Helpers/NotificationMessageBuildersTest.cs
+++ Messenger/Messenger.Core.Tests.MSTest/Helpers/NotificationMessageBuildersTest.cs
@@ -14,7 +14,6 @@ namespace Messenger.Tests.MSTest
     {
         public void MakeMessageInSubscribedChannelNotificationMessage_Test()
         {
-
             var testName = System.Reflection.MethodBase.GetCurrentMethod().Name;
 
             Task.Run(async () =>
@@ -46,7 +45,41 @@ namespace Messenger.Tests.MSTest
                 Assert.AreEqual(notificationMessageReference, notificationMessageBuilt);
 
             }).GetAwaiter().GetResult();
+        }
 
+        public void MakeMessageInSubscribedTeamNotificationMessage_Test()
+        {
+            var testName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
+            Task.Run(async () =>
+            {
+                string userId = (await UserService.GetOrCreateApplicationUser(new User(){Id = testName + "User"})).Id;
+                Assert.IsNotNull(userId);
+                Assert.AreNotEqual("",userId);
+
+                uint? teamId = await TeamService.CreateTeam(testName + "Team");
+                Assert.IsNotNull(teamId);
+
+                uint? channelId = await ChannelService.CreateChannel(testName + "Chanel", teamId.Value);
+                Assert.IsNotNull(channelId);
+
+                uint? messageId = await MessageService.CreateMessage(channelId.Value, userId, testName + "Message");
+                Assert.IsNotNull(messageId);
+
+                var notificationMessageBuilt = NotificationMessageBuilder.MakeMessageInSubscribedTeamNotificationMessage(messageId.Value);
+
+                var notificationMessageReference = new JObject()
+                {
+                    {"notificationType",   NotificationType.MessageInSubscribedChannel.ToString()},
+                    {"notificationSource", NotificationSource.Channel.ToString()},
+                    {"senderName",         testName + "User"},
+                    {"channelName",        testName + "Channel"},
+                    {"teamName",           testName + "Team"}
+                };
+
+                Assert.AreEqual(notificationMessageReference, notificationMessageBuilt);
+
+            }).GetAwaiter().GetResult();
         }
     }
 }
