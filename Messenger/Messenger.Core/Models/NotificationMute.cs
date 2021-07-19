e3400dfc8e5bf0a91b455b63afc3cee101458644
jonas <jonasmuehlmann@protonmail.com>
2021-07-19 18:27:06 +0200

    feat(`NotificationMute`): add properties `UserId` and rename other properties

--- Messenger/Messenger.Core/Models/NotificationMute.cs
+++ Messenger/Messenger.Core/Models/NotificationMute.cs
@@ -2,19 +2,23 @@ namespace Messenger.Core.Models
 {
     public class NotificationMute
     {
-        public uint               Id       { get; set; }
-        public NotificationType   Type     { get; set; }
-        public NotificationSource Source   { get; set; }
-        public string             SenderId { get; set; }
+        public uint               Id                      { get; set; }
+        public NotificationType   NotificationType        { get; set; }
+        public NotificationSource NotificationSourceType  { get; set; }
+        public string             NotificationSourceValue { get; set; }
+        public string             SenderId                { get; set; }
+        public string             UserId                  { get; set; }
 
         public NotificationMute()
         {
+            NotificationSourceValue = "";
             SenderId = "";
+            UserId = "";
         }
 
         public override string ToString()
         {
-            return $"NotificationMute: Id={Id} Type={Type.ToString()} Source={Source.ToString()}, SenderId={SenderId}";
+            return $"NotificationMute: Id={Id} NotificationType={NotificationType.ToString()} NotificationSourceType={NotificationSourceType.ToString()} NotificationSourceValue={NotificationSourceValue}, SenderId={SenderId}, UserId={UserId}";
         }
     }
 }
