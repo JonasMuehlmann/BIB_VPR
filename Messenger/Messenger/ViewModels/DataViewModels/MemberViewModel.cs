fbfa9ef91145de81dd55de666a91913b0778842a
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-22 17:36:29 +0100

    renamed Member to MemberViewModel

--- Messenger/Messenger/ViewModels/DataViewModels/MemberViewModel.cs
+++ Messenger/Messenger/ViewModels/DataViewModels/MemberViewModel.cs
@@ -1,20 +1,30 @@
-﻿using Messenger.ViewModels.DataViewModels;
+﻿using Messenger.Core.Models;
+using Messenger.Models;
+using Messenger.ViewModels.DataViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Messenger.Models
+namespace Messenger.ViewModels.DataViewModels
 {
-    public class Member : UserViewModel
+    public class MemberViewModel : UserViewModel
     {
         private List<MemberRole> _memberRoles = new List<MemberRole>();
+
+        private uint _teamId;
 
         public List<MemberRole> MemberRoles
         {
             get { return _memberRoles; }
             set { Set(ref _memberRoles, value); }
+        }
+
+        public uint TeamId
+        {
+            get { return _teamId; }
+            set { Set(ref _teamId, value); }
         }
     }
 }
