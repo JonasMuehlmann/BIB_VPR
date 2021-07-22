8b26a367a120d13b57df1323e5b91a2bef4cbe5a
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-22 17:31:57 +0100

    added SignalREventArgs

---
+++ Messenger/Messenger.Core/Models/SignalREventArgs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Messenger.Core.Models
+{
+    public class SignalREventArgs<T> : EventArgs
+    {
+        public T Value { get; set; }
+
+        public SignalREventArgs(T value)
+        {
+            Value = value;
+        }
+    }
+
+    public class SignalREventArgs<TOne, TTwo> : EventArgs
+    {
+        public TOne FirstValue { get; set; }
+
+        public TTwo SecondValue { get; set; }
+
+        public SignalREventArgs(TOne firstValue, TTwo secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+        }
+    }
+}
