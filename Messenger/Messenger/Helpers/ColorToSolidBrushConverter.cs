782eb3196e478c04fefc9c00bb4af282e0fe125e
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-22 23:56:52 +0100

    added ColorToSolidBrushConverter

---
+++ Messenger/Messenger/Helpers/ColorToSolidBrushConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+namespace Messenger.Helpers
+{
+    public class ColorToSolidBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is Color)
+            {
+                return new SolidColorBrush((Color)value);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return null;
+        }
+    }
+}
