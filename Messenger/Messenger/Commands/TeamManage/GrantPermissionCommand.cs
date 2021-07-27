f852f11c3e978e855dc1f36ff5989ac3a2145e33
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-27 21:24:21 +0100

    implemented grant/revoke permission commands

---
+++ Messenger/Messenger/Commands/TeamManage/GrantPermissionCommand.cs
@@ -0,0 +1,56 @@
+﻿using Messenger.Core.Models;
+using Messenger.Core.Services;
+using Messenger.ViewModels.DataViewModels;
+using Messenger.ViewModels.DialogBoxes;
+using Messenger.Views.DialogBoxes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Messenger.Commands.TeamManage
+{
+    public class GrantPermissionCommand : ICommand
+    {
+        private ManageRolesDialogViewModel _vm;
+
+        public event EventHandler CanExecuteChanged;
+
+        public GrantPermissionCommand(ManageRolesDialogViewModel vm)
+        {
+            _vm = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            bool executable = parameter != null
+                && parameter is Permissions
+                && _vm != null
+                && _vm.SelectedTeamRole != null;
+
+            if (!executable) return;
+
+            try
+            {
+                TeamViewModel currentTeam = App.StateProvider.SelectedTeam;
+                TeamRoleViewModel selectedRole = _vm.SelectedTeamRole;
+                Permissions permission = (Permissions)parameter;
+
+                bool isSuccess = await MessengerService.GrantPermission(currentTeam.Id, selectedRole.Title, permission);
+            }
+            catch (Exception e)
+            {
+                await ResultConfirmationDialog
+                    .Set(false, $"Error while granting a permission to a role: {e.Message}")
+                    .ShowAsync();
+            }
+        }
+    }
+}
---
+++ Messenger/Messenger/Commands/TeamManage/RevokePermissionCommand.cs
@@ -0,0 +1,54 @@
+﻿using Messenger.Core.Models;
+using Messenger.Core.Services;
+using Messenger.ViewModels.DataViewModels;
+using Messenger.ViewModels.DialogBoxes;
+using Messenger.Views.DialogBoxes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Messenger.Commands.TeamManage
+{
+    public class RevokePermissionCommand : ICommand
+    {
+        private ManageRolesDialogViewModel _vm;
+
+        public event EventHandler CanExecuteChanged;
+
+        public RevokePermissionCommand(ManageRolesDialogViewModel vm)
+        {
+            _vm = vm;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            bool executable = parameter != null
+                && parameter is Permissions;
+
+            if (!executable) return;
+
+            try
+            {
+                TeamViewModel currentTeam = App.StateProvider.SelectedTeam;
+                TeamRoleViewModel selectedRole = _vm.SelectedTeamRole;
+                Permissions permission = (Permissions)parameter;
+
+                bool isSuccess = await MessengerService.RevokePermission(currentTeam.Id, selectedRole.Title, permission);
+            }
+            catch (Exception e)
+            {
+                await ResultConfirmationDialog
+                    .Set(false, $"Error while revoking a permission from a role: {e.Message}")
+                    .ShowAsync();
+            }
+        }
+    }
+}
