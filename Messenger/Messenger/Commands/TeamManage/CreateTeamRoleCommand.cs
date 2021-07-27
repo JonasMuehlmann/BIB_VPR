7e40c098e8251e4d663c5d201adbe05d27d5b510
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-27 19:36:07 +0100

    added CreateTeamRoleCommand

---
+++ Messenger/Messenger/Commands/TeamManage/CreateTeamRoleCommand.cs
@@ -0,0 +1,55 @@
+﻿using Messenger.Core.Services;
+using Messenger.ViewModels.DataViewModels;
+using Messenger.ViewModels.DialogBoxes;
+using Messenger.Views.DialogBoxes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Messenger.Commands.TeamManage
+{
+    public class CreateTeamRoleCommand : ICommand
+    {
+        private ManageRolesDialogViewModel _vm;
+
+        public CreateTeamRoleCommand(ManageRolesDialogViewModel vm)
+        {
+            _vm = vm;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public async void Execute(object parameter)
+        {
+            bool executable = parameter != null
+                && parameter is string;
+
+            try
+            {
+                TeamViewModel currentTeam = App.StateProvider.SelectedTeam;
+                string roleTitle = parameter.ToString();
+
+                bool isSuccess = await MessengerService.CreateTeamRole(roleTitle, currentTeam.Id);
+
+                if (isSuccess)
+                {
+                    _vm.NewRoleTitle = string.Empty;
+                }
+            }
+            catch (Exception e)
+            {
+                await ResultConfirmationDialog
+                    .Set(false, $"Error while creating a new team role: {e.Message}")
+                    .ShowAsync();
+            }
+        }
+    }
+}
