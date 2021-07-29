5534980352bf73a21e9e7e7aead37d47fd019cce
jonas <jonasmuehlmann@protonmail.com>
2021-07-29 12:09:28 +0200

    feat(`EmojiUtils`): add variable and methods to filter emojis by categorie

--- Messenger/Messenger.Core/Helpers/EmojiUtils.cs
+++ Messenger/Messenger.Core/Helpers/EmojiUtils.cs
@@ -10,11 +10,28 @@ namespace Messenger.Core.Helpers
     public class EmojiPicker
     {
         public List<Emoji> emojis;
+        public EmojiCategorieFilter emojiCategorieFilter;
 
         public EmojiPicker(string emojiFilePath)
         {
             string fileContent = File.ReadAllText(emojiFilePath);
             emojis = JsonConvert.DeserializeObject<Dictionary<string, List<Emoji>>>(fileContent)["emojis"];
+            emojiCategorieFilter = EmojiCategorieFilter.None;
+        }
+
+        public void AddFilter(EmojiCategorieFilter filter)
+        {
+            if (!emojiCategorieFilter.HasFlag(filter))
+            {
+                emojiCategorieFilter |= filter;
+            }
+        }
+        public void RemoveFilter(EmojiCategorieFilter filter)
+        {
+            if (emojiCategorieFilter.HasFlag(filter))
+            {
+                emojiCategorieFilter ^= filter;
+            }
         }
     }
     public class EmojiUtils
