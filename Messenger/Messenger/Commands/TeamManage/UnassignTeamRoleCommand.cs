734b9cceb35c52be29e4b3794d2111c4fb06d169
Jay Kim <jawoon.kim@edu.bib.de>
2021-07-30 19:56:12 +0100

    modified TeamRoleCommands to receive correct view model

--- Messenger/Messenger/Commands/TeamManage/UnassignTeamRoleCommand.cs
+++ Messenger/Messenger/Commands/TeamManage/UnassignTeamRoleCommand.cs
@@ -2,6 +2,7 @@
 using Messenger.Core.Services;
 using Messenger.ViewModels;
 using Messenger.ViewModels.DataViewModels;
+using Messenger.ViewModels.DialogBoxes;
 using Messenger.Views.DialogBoxes;
 using System;
 using System.Windows.Input;
@@ -10,11 +11,11 @@ namespace Messenger.Commands.TeamManage
 {
     public class UnassignTeamRoleCommand : ICommand
     {
-        private TeamManageViewModel _vm;
+        private ManageMemberRolesDialogViewModel _vm;
 
         public event EventHandler CanExecuteChanged;
 
-        public UnassignTeamRoleCommand(TeamManageViewModel vm)
+        public UnassignTeamRoleCommand(ManageMemberRolesDialogViewModel vm)
         {
             _vm = vm;
         }
@@ -27,7 +28,7 @@ namespace Messenger.Commands.TeamManage
         public async void Execute(object parameter)
         {
             bool executable = parameter != null
-                && parameter is TeamRole;
+                && parameter is TeamRoleViewModel;
 
             if (!executable) return;
 
@@ -35,7 +36,7 @@ namespace Messenger.Commands.TeamManage
             {
                 TeamViewModel currentTeam = App.StateProvider.SelectedTeam;
                 TeamRoleViewModel selectedRole = (TeamRoleViewModel)parameter;
-                MemberViewModel targetMember = _vm.SelectedMember;
+                MemberViewModel targetMember = _vm.Member;
 
                 bool isSuccess = await MessengerService.UnassignUserRole(selectedRole.Title, targetMember.Id, currentTeam.Id);
             }
