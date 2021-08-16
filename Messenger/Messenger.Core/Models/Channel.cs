8ec442d1bec9e379d769a2d6f9c657734f16c156
jonas <jonasmuehlmann@protonmail.com>
2021-08-16 10:11:24 +0200

    docs: add `README.md` files to explain directory structure

--- Messenger/Messenger.Core/Models/Channel.cs
+++ Messenger/Messenger.Core/Models/Channel.cs
@@ -1,14 +1,29 @@
-
 namespace Messenger.Core.Models
 {
+
+    /// <summary>
+    /// The source code side representation of a team's channel
+    /// </summary>
     public class Channel
     {
+        /// <summary>
+        /// The unique id  of the channel
+        /// </summary>
         public uint ChannelId { get; set; }
 
+        /// <summary>
+        /// The name of the channel
+        /// </summary>
         public string ChannelName { get; set; }
 
+        /// <summary>
+        /// The id of the team the channel belongs to
+        /// </summary>
         public uint TeamId { get; set; }
 
+        /// <summary>
+        /// Default initialize all members(string values get initialized to "" instead of null)
+        /// </summary>
         public Channel()
         {
             ChannelName = "";
