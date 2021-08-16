e588aa37e7414b9d0b1f3889b27f0bf9a3572d81
jonas <jonasmuehlmann@protonmail.com>
2021-08-16 15:49:25 +0200

    tests(`FileSharingService`): fix tests after refactor

--- Messenger/Messenger.Core.Tests.MSTest/Services/FileSharingServiceTest.cs
+++ Messenger/Messenger.Core.Tests.MSTest/Services/FileSharingServiceTest.cs
@@ -1,7 +1,9 @@
 using System.Threading.Tasks;
+using System;
 using System.IO;
+using System.Text;
 using Messenger.Core.Services;
-
+using Messenger.Core.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 
@@ -10,18 +12,32 @@ namespace Messenger.Tests.MSTest
     [TestClass]
     public class FileSharingServiceTest
     {
-
         [TestMethod]
         public void UploadDownload_test()
         {
+            var testName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+
             Task.Run(async () =>
             {
-                string fileName = await FileSharingService.Upload(Path.GetTempFileName());
+                var unicodeEncoding = new UnicodeEncoding();
+                var streamContent   = unicodeEncoding.GetBytes(testName + "Content");
+                var memStream       = new MemoryStream(streamContent.Length);
 
-                bool success = await FileSharingService.Download(fileName);
+                memStream.Write(streamContent, 0, streamContent.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
 
-                Assert.IsNotNull(fileName);
-                Assert.IsTrue(success);
+                var blobFilename = await FileSharingService.Upload(new UploadData(memStream, $"{Path.GetTempPath()}{testName}File.txt"));
+                Assert.IsNotNull(blobFilename);
+
+                var resultStream = await FileSharingService.Download(blobFilename);
+                Assert.IsNotNull(resultStream);
+
+                var resultContent = resultStream.ToArray();
+                resultStream.Read(resultContent , 0, streamContent.Length);
+                Assert.AreEqual(BitConverter.ToString(streamContent), BitConverter.ToString(resultContent));
+
+                var didDelete = await FileSharingService.Delete(blobFilename);
+                Assert.IsTrue(didDelete);
 
             }).GetAwaiter().GetResult();
         }
