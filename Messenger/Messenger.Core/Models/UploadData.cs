7b5d169659f806404e701a3e65d04443d51ff364
jonas <jonasmuehlmann@protonmail.com>
2021-08-16 14:41:08 +0200

    feat(`Core.Models.UploadData`): add docstrings

--- Messenger/Messenger.Core/Models/UploadData.cs
+++ Messenger/Messenger.Core/Models/UploadData.cs
@@ -5,11 +5,23 @@ using System.Text;
 
 namespace Messenger.Core.Models
 {
+    /// <summary>
+    /// Holds a Stream of data and a file path intended to safe the stream's data to
+    /// </summary>
     public class UploadData
     {
+        /// <summary>
+        /// Holds a stream of data intended as a files content
+        /// </summary>
         public Stream StreamFile { get; set; }
+        /// <summary>
+        /// Holds A file path intended to safe the stream's data to
+        /// </summary>
         public string FilePath { get; set; }
 
+        /// <summary>
+        /// Fully initialize an UploadData instance
+        /// </summary>
         public UploadData(Stream streamFile, string filePath)
         {
             StreamFile = streamFile;
