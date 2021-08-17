b8549a36e2d85bb98045e22e85cf083147a5e34b
Jay Kim <jawoon.kim@edu.bib.de>
2021-08-17 19:47:14 +0100

    StateProvider loads all notifications from the database upon initialization

--- Messenger/Messenger/Services/Providers/StateProvider.cs
+++ Messenger/Messenger/Services/Providers/StateProvider.cs
@@ -1,4 +1,5 @@
 ﻿using Messenger.Core.Helpers;
+using Messenger.Core.Models;
 using Messenger.Core.Services;
 using Messenger.Helpers;
 using Messenger.Helpers.MessageHelpers;
@@ -6,9 +7,10 @@ using Messenger.Helpers.TeamHelpers;
 using Messenger.Models;
 using Messenger.ViewModels.DataViewModels;
 using Microsoft.Toolkit.Uwp.Notifications;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Threading.Tasks;
-using Windows.UI.Notifications;
 
 namespace Messenger.Services.Providers
 {
@@ -21,6 +23,8 @@ namespace Messenger.Services.Providers
         private ChannelViewModel _selectedChannel;
 
         private UserViewModel _currentUser;
+
+        private ObservableCollection<NotificationViewModel> _notifications;
 
         #endregion
 
@@ -63,6 +67,12 @@ namespace Messenger.Services.Providers
             set { Set(ref _currentUser, value); }
         }
 
+        public ObservableCollection<NotificationViewModel> Notifications
+        {
+            get { return _notifications; }
+            set { Set(ref _notifications, value); }
+        }
+
         #endregion
 
         public StateProvider()
@@ -95,8 +105,10 @@ namespace Messenger.Services.Providers
             Singleton<ToastNotificationsService>.Instance.UpdateInitialization(2);
 
             await provider.LoadAllMessages();
+            await provider.LoadAllNotifications();
 
             Singleton<ToastNotificationsService>.Instance.UpdateInitialization(3);
+
 
             /* BROADCAST MY TEAMS */
             App.EventProvider.Broadcast(
@@ -128,6 +140,21 @@ namespace Messenger.Services.Providers
             if (provider.TeamManager.MyChats.Count() > 0)
             {
                 await provider.MessageManager.CreateEntry(provider.TeamManager.MyChats);
+            }
+        }
+
+        public static async Task LoadAllNotifications(this StateProvider provider)
+        {
+            provider.Notifications = new ObservableCollection<NotificationViewModel>();
+
+            IEnumerable<Notification> data = await NotificationService.RetrieveNotifications(provider.CurrentUser.Id);
+
+            if (data != null && data.Count() > 0)
+            {
+                foreach (Notification item in data)
+                {
+                    provider.Notifications.Add(new NotificationViewModel(item));
+                }
             }
         }
     }
