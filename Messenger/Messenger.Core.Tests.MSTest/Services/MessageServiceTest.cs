dd83007fbfb2d942b944cadc74fa73682653b63b
jonas <jonasmuehlmann@protonmail.com>
2021-08-22 19:45:21 +0200

    refactor(Core.Tests): refactor data generator methods into own class

--- Messenger/Messenger.Core.Tests.MSTest/Services/MessageServiceTest.cs
+++ Messenger/Messenger.Core.Tests.MSTest/Services/MessageServiceTest.cs
@@ -16,115 +16,6 @@ namespace Messenger.Tests.MSTest
     [TestClass]
     public class MessageServiceTest
     {
-        public async Task<User> DefaultUserGenerator(string entityNamePrefix)
-        {
-            return await UserService.GetOrCreateApplicationUser(new User(){
-                    Id          = entityNamePrefix + "User",
-                    DisplayName = entityNamePrefix + "DisplayName",
-                    NameId      = 0,
-                    Photo       = entityNamePrefix + "Photo",
-                    Mail        = entityNamePrefix + "Mail",
-                    Bio         = entityNamePrefix + "Bio"
-                    });
-
-
-        }
-        
-        public async Task<Team> DefaultTeamGenerator(string entityNamePrefix)
-        {
-            var teamId = await TeamService.CreateTeam(entityNamePrefix + "Team", entityNamePrefix + "Description");
-
-            if (teamId is null)
-            {
-                return null;
-            }
-            return await TeamService.GetTeam(teamId.Value);
-        }
-        
-        public async Task<Channel> DefaultChannelGenerator(string entityNamePrefix, uint teamId)
-        {
-            var channelId = await ChannelService.CreateChannel(entityNamePrefix + "Channel", teamId);
-
-            if (channelId is null)
-            {
-                return null;
-            }
-            return await ChannelService.GetChannel(channelId.Value);
-        }
-        
-        public async Task<Message> DefaultMessageGenerator(string entityNamePrefix, uint recipientId, string senderId)
-        {
-            var messageId = await MessageService.CreateMessage(recipientId, senderId, entityNamePrefix + "Message");
-
-            if (messageId is null)
-            {
-                return null;
-            }
-            return await MessageService.GetMessage(messageId.Value);
-        }
-        
-        public async Task<Reaction> DefaultReactionGenerator(uint messageId, string userId)
-        {
-            var reactionId = await MessageService.AddReaction(messageId, userId, "+");
-
-            return await MessageService.GetReaction(reactionId);
-        }
-
-        public async Task<Dictionary<string, dynamic>> SetupData(
-               string entityNamePrefix,
-               Func<string, Task<User>> alternateUserGenerator     = null,
-               Func<string, Task<Team>> alternateTeamGenerator     = null,
-               Func<string, uint, Task<Channel>> alternateChannelGenerator  = null,
-               Func<string, uint, string, Task<Message>> alternateMessageGenerator  = null,
-               Func<uint, string, Task<Reaction>> alternateReactionGenerator =  null
-            )
-        {
-            Dictionary<string, dynamic> data = new Dictionary<string, dynamic>();
-
-            if (alternateUserGenerator is null)
-            {
-                data["User"] = await DefaultUserGenerator(entityNamePrefix);
-            }
-            else
-            {
-                data["User"] = await  alternateUserGenerator(entityNamePrefix);
-            }
-            if (alternateTeamGenerator is null)
-            {
-                data["Team"] = await  DefaultTeamGenerator(entityNamePrefix);
-            }
-            else
-            {
-                data["Team"] = await  alternateTeamGenerator(entityNamePrefix);
-            }
-            if (alternateChannelGenerator is null)
-            {
-                data["Channel"] = await  DefaultChannelGenerator(entityNamePrefix, data["Team"].Id);
-            }
-            else
-            {
-                data["Channel"] = await  alternateChannelGenerator(entityNamePrefix, data["Team"].Id);
-            }
-            if (alternateMessageGenerator is null)
-            {
-                data["Message"] = await  DefaultMessageGenerator(entityNamePrefix, data["Channel"].ChannelId, data["User"].Id);
-            }
-            else
-            {
-                data["Message"] = await  alternateMessageGenerator(entityNamePrefix, data["Channel"].ChannelId, data["User"].Id);
-            }
-            if (alternateReactionGenerator is null)
-            {
-                data["Reaction"] = await  DefaultReactionGenerator(data["Message"].Id, data["User"].Id);
-            }
-            else
-            {
-                data["Reaction"] = await  alternateReactionGenerator(data["Message"].Id, data["User"].Id);
-            }
-            
-            return data;
-        }
-        
         [TestMethod]
         public void CreateMessage_Test()
         {
@@ -132,7 +23,7 @@ namespace Messenger.Tests.MSTest
 
             Task.Run(async () =>
             {
-                var data = await SetupData(testName);
+                var data = await DataGenerator.SetupData(testName);
                 
                 var result = await MessageService.CreateMessage(data["Channel"].ChannelId, data["User"].Id, data["Message"].Content);
                 Assert.IsNotNull(result);
