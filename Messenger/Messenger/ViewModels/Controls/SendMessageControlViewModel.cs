6f1d1238f44d36014251b6efff4768efca04eced
Jay Kim <jawoon.kim@edu.bib.de>
2021-09-04 18:58:56 +0100

    SendMessageControlViewModel provides EmojiPicker instance

--- Messenger/Messenger/ViewModels/Controls/SendMessageControlViewModel.cs
+++ Messenger/Messenger/ViewModels/Controls/SendMessageControlViewModel.cs
@@ -1,7 +1,11 @@
 ﻿using Messenger.Commands.Messenger;
+using Messenger.Core.Helpers;
+using Messenger.Core.Models;
+using Messenger.Helpers;
 using Messenger.ViewModels.Pages;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,9 +13,21 @@ using System.Windows.Input;
 
 namespace Messenger.ViewModels.Controls
 {
-    public class SendMessageControlViewModel
+    public class SendMessageControlViewModel : Observable
     {
         public ChatViewModel ParentViewModel { get; set; }
+
+        public EmojiPicker EmojiPicker { get; set; }
+
+        private ObservableCollection<Emoji> _emojis;
+
+        public ObservableCollection<Emoji> Emojis
+        {
+            get { return _emojis; }
+            set { Set(ref _emojis, value); }
+        }
+
+        public IEnumerable<string> EmojiCategories { get => Enum.GetNames(typeof(EmojiCategory)); }
 
         public ICommand AttachFileCommand { get => new AttachFileCommand(ParentViewModel); }
 
@@ -20,6 +36,9 @@ namespace Messenger.ViewModels.Controls
         public SendMessageControlViewModel(ChatViewModel parentViewModel)
         {
             ParentViewModel = parentViewModel;
+            EmojiPicker = new EmojiPicker("");
+
+            Emojis = new ObservableCollection<Emoji>(EmojiPicker.GetEmojisFromCategory(EmojiCategory.Smileys));
         }
     }
 }
