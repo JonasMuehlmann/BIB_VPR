b2928682b9993f2bf0ba4420d409a96792be5ac4
Jay Kim <jawoon.kim@edu.bib.de>
2021-09-05 10:08:55 +0100

    Implemented EmojiPicker in SendMessageControl

--- Messenger/Messenger/Views/Controls/SendMessageControl.xaml.cs
+++ Messenger/Messenger/Views/Controls/SendMessageControl.xaml.cs
@@ -1,6 +1,10 @@
-﻿using Messenger.ViewModels.Controls;
+﻿using Messenger.Core.Models;
+using Messenger.ViewModels.Controls;
+using System;
+using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Input;
 
 namespace Messenger.Views.Controls
@@ -16,9 +20,15 @@ namespace Messenger.Views.Controls
         public static readonly DependencyProperty ViewModelProperty =
             DependencyProperty.Register("ViewModel", typeof(SendMessageControlViewModel), typeof(SendMessageControl), new PropertyMetadata(null));
 
+        private DispatcherTimer _timer;
+        private string _searchTerm;
+
         public SendMessageControl()
         {
             InitializeComponent();
+            _timer = new DispatcherTimer();
+            _timer.Interval = TimeSpan.FromMilliseconds(300);
+            _timer.Tick += OnSearchTimer;
         }
 
         private void tbxContent_PreviewKeyDown(object sender, KeyRoutedEventArgs e)
@@ -41,6 +51,45 @@ namespace Messenger.Views.Controls
         {
             ViewModel.ParentViewModel.ReplyMessage = null;
             ViewModel.ParentViewModel.MessageToSend.ParentMessageId = null;
+        }
+
+        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        {
+            string emojiString = (sender as ToggleButton).Content as string;
+
+            ViewModel.AddEmojiFilter((EmojiCategory)Enum.Parse(typeof(EmojiCategory), emojiString));
+        }
+
+        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
+        {
+            string emojiString = (sender as ToggleButton).Content as string;
+
+            ViewModel.RemoveEmojiFilter((EmojiCategory)Enum.Parse(typeof(EmojiCategory), emojiString));
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string term = (sender as TextBox).Text;
+
+            if (string.IsNullOrWhiteSpace(term) || term.Length <= 2) return;
+
+            _searchTerm = term;
+
+            if (_timer.IsEnabled)
+            {
+                _timer.Stop();
+            }
+
+            _timer.Start();
+        }
+
+        private void OnSearchTimer(object sender, object e)
+        {
+            if (string.IsNullOrWhiteSpace(_searchTerm)) return;
+
+            ViewModel.SearchEmojis(_searchTerm);
+
+            _timer.Stop();
         }
     }
 }
