d39b0176aee893b84091beeee8dc0cdd3373702c
Jay Kim <jawoon.kim@edu.bib.de>
2021-09-05 10:08:01 +0100

    extended NullToVisilbilityConvertert to check count is zero

--- Messenger/Messenger/Helpers/Converters/NullToVisibilityConverter.cs
+++ Messenger/Messenger/Helpers/Converters/NullToVisibilityConverter.cs
@@ -13,6 +13,18 @@ namespace Messenger.Helpers.Converters
     {
         public object Convert(object value, Type targetType, object parameter, string language)
         {
+            if (value != null && int.TryParse(value.ToString(), out int count))
+            {
+                if (parameter != null && parameter.ToString() == "invert")
+                {
+                    return count == 0 ? Visibility.Visible : Visibility.Collapsed;
+                }
+                else
+                {
+                    return count == 0 ? Visibility.Collapsed : Visibility.Visible;
+                }
+            }
+
             bool isNull = value == null;
             bool emptyString = false;
             bool emptyList = false;
